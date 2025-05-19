09394178369509a4ddec7c0be4d86a1d4f3fa87e
AndreaRispoli97 <Andrea9715@outlook.it>
2025-05-19 18:28:34 +0200

    feat: add FormTrip component for trip management and enhance DetailUser and Home pages with participant search functionality

--- src/pages/DetailTrip.jsx
+++ src/pages/DetailTrip.jsx
@@ -1,51 +1,50 @@
 import { useParams } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import TripContext from "../context/TripContext";
 import CardTrip from "../components/CardTrip";
 
 function TripDetails() {
-    const { id } = useParams();
-    const { ArrayTrip } = useContext(TripContext);
+  const { id } = useParams();
+  const { ArrayTrip } = useContext(TripContext);
 
-    return (
-        <>
-            <div className="container my-5">
-                {ArrayTrip.map((trip) =>
-                    trip.id.toString() === id ? (
-                        <div key={trip.id} className="card shadow">
-                            <div className="row g-0">
-                                <div className="col-md-6">
-                                    <img
-                                        src={`/public/${trip.image}`}
-                                        alt={trip.destination}
-                                        className="img-fluid rounded-start h-100 object-fit-cover"
-                                    />
-                                </div>
-                                <div className="col-md-6">
-                                    <div className="card-body">
-                                        <h2 className="card-title">{trip.destination}</h2>
-                                        <p className="card-text">
-                                            <strong>Operatore:</strong> {trip.operator}
-                                        </p>
-                                        <p className="card-text">
-                                            <strong>Stato:</strong> {trip.status}
-                                        </p>
-                                        <p className="card-text">
-                                            <strong>Partenza:</strong> {trip.start_date}
-                                        </p>
-                                        <p className="card-text">
-                                            <strong>Ritorno:</strong> {trip.end_date}
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                            <CardTrip />
-                        </div>
+  const [tripList, setTripList] = useState(ArrayTrip);
 
-                    ) : null
-                )}
+  return (
+    <div className="container my-5">
+      {tripList.map((trip) =>
+        trip.id.toString() === id ? (
+          <div key={trip.id} className="card shadow">
+            <div className="row g-0">
+              <div className="col-md-6">
+                <img
+                  src={`/${trip.image}`}
+                  alt={trip.destination}
+                  className="img-fluid rounded-start h-100 object-fit-cover"
+                />
+              </div>
+              <div className="col-md-6">
+                <div className="card-body">
+                  <h2 className="card-title">{trip.destination}</h2>
+                  <p className="card-text">
+                    <strong>Operatore:</strong> {trip.operator}
+                  </p>
+                  <p className="card-text">
+                    <strong>Stato:</strong> {trip.status}
+                  </p>
+                  <p className="card-text">
+                    <strong>Partenza:</strong> {trip.start_date}
+                  </p>
+                  <p className="card-text">
+                    <strong>Ritorno:</strong> {trip.end_date}
+                  </p>
+                </div>
+              </div>
             </div>
-        </>
-    );
+            <CardTrip />
+          </div>
+        ) : null
+      )}
+    </div>
+  );
 }
 export default TripDetails;
